a8fc86b989db6eb0ea066c04c4e36145fddcebdb
Anoop S S <anoopss@schoolspeak.com>
2016-04-27 18:07:21 +0530

    initial commit

---
+++ app/src/main/java/co/cyware/ftpclient/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package co.cyware.ftpclient.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import co.cyware.ftpclient.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
